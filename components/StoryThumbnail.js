ecd5089141835d4a655a2d7db839b928fa10c196
bhola <bhola2266@gmail.com>
2022-03-27 00:26:11 +0545

    commit

--- components/StoryThumbnail.js
+++ components/StoryThumbnail.js
@@ -12,24 +12,21 @@ function StoryThumbnail({ story_details }) {
 
     const router = useRouter()
 
-    const onClickHandler = () => {
+    const rough = story_details.href.substring(story_details.href.indexOf('.com/') + 5, story_details.href.length - 1)
+    const category = rough.substring(0, rough.indexOf('/'))
+    const title = rough.substring(rough.indexOf('/') + 1, rough.length)
 
-        const rough = story_details.href.substring(story_details.href.indexOf('.com/') + 5, story_details.href.length - 1)
-        const category = rough.substring(0, rough.indexOf('/'))
-        const title = rough.substring(rough.indexOf('/') + 1, rough.length)
 
-        // router.push({
-        //     pathname: `/${category}/${title}`,
-        //     query: { link: story_details.href }
-        // })
 
-        router.push(`/${category}/${title}`)
-    }
 
 
     return (
         <div className=" p-4 bg-orange-100 border-2 border-gray-400 m-2 shadow rounded-lg "  >
-            <h2 onClick={onClickHandler} className='text-xl font-semibold text-orange-800 cursor-pointer hover:text-green-800  '>{story_details.Title}</h2>
+            <Link href={`/${category}/${title}`}>
+                <a>
+                    <h2 className='text-xl font-semibold text-orange-800 cursor-pointer hover:text-green-800  '>{story_details.Title}</h2>
+                </a>
+            </Link>
             <div className='flex items-center text-sm  my-2 space-x-2'>
 
                 <div className='flex  items-center '>
@@ -47,14 +44,19 @@ function StoryThumbnail({ story_details }) {
 
             <p className='text-gray-800 text-lg'>{story_details.description}</p>
 
-
-            <p onClick={onClickHandler} className=' mb-1 font-semibold text-md text-right text-orange-800 hover:text-green-800 cursor-pointer'>पूरी कहानी पढ़ें</p>
+            <Link href={`/${category}/${title}`}>
+                <a>
+                    <p className=' mb-1 font-semibold text-md text-right text-orange-800 hover:text-green-800 cursor-pointer'>पूरी कहानी पढ़ें</p>
+                </a>
+            </Link>
 
 
             <div className='flex flex-wrap space-x-1'>
 
                 {story_details.tags.map(tag => {
                     return (
+
+
                         <p onClick={() => { router.push(`/tag/${tag.href.substring(tag.href.indexOf('tag/') + 4, tag.href.length - 1)}`) }} className='hover:text-red-800 cursor-pointer bg-yellow-100 border-2 rounded text-xs m-1 border-gray-500 hover:bg-yellow-200 ' key={tag.name}>{tag.name}</p>
                     )
                 })}
