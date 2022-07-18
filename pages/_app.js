a7340b15943ea103b126bbd2ad9e89341d105efd
baldgenie <siddhartha.kapoor@gmail.com>
2022-07-18 22:22:02 +0545

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -25,16 +25,25 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
 
-      <Head>
-        <title>Desi Porn</title>
+      {/* <Head>
+        <meta name='asg_verification' content='vVcWCcbbgmnqv221hpAjPojb' />
+        <meta name="exoclick-site-verification" content="6b1112fe173bdf782d96975e70bd4b95"></meta>
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="theme-color" content="#ffffff" />  <meta name="hilltopads-site-verification" content="28c33bbf5c61164c74b2bdfcc2ff1d44bc9c45b5" />
+      </Head> */}
 
-      </Head>
 
       <VideoState>
 
         <Navbar />
-        <div className='flex'>
+        <div className='flex lg:w-11/12 lg:mx-auto mt-1 md:mt-3  mx-2 md:mx-4'>
           <Sidebar />
           <Component {...pageProps} />
         </div>
